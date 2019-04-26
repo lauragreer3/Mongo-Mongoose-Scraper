9200926b74dad36adc76324bea78d399178450dd
Moonkitten <lauragreer3@gmail.com>
2019-04-25 19:41:55 -0600

    created save method

--- app/data/article_schema.js
+++ app/data/article_schema.js
@@ -13,4 +13,33 @@ var articleSchema = new Schema({
         email: String,
         date_added: { type: Date, default: Date.now}
     }]
-})
+});
+
+var Article = mongoose.model('Article', articleSchema);
+
+articleSchema.methods.findDuplicateArticle = function(article, cb) {
+    //check if the article is already in the database
+    results = this.model('Article').find({ type: this.type, headline: article.headline }, cb);
+    //if not, save to the database
+};
+
+articleSchema.methods.saveArticle = function(article, cb) {
+    var NewArticle = new Article({
+        headline: article.headline
+        author: article.author, 
+        url: article.url,
+        body: article.body,
+        date_published: article.date_published
+    });
+    NewArticle.save(function(err) {
+        if (err) {
+            console.log('Error');
+            console.log(err);
+        }
+        else
+        {
+            console.log('Article Saved');
+            conslog.log(NewArticle);
+        }
+    })
+};
